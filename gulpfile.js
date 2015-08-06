6de9f83296e7826f7e1ccc56c7c6be1ab4b67b94
malles <matthijs@bixie.nl>
2015-08-06 17:19:20 +0200

    compile less

--- gulpfile.js
+++ gulpfile.js
@@ -6,22 +6,20 @@
  * lint: runs jshint on all .js files
  */
 
-var merge      = require('merge-stream'),
-    gulp       = require('gulp'),
-    header     = require('gulp-header'),
-    less       = require('gulp-less'),
-    rename     = require('gulp-rename'),
-    eslint     = require('gulp-eslint');
+var merge = require('merge-stream'),
+    gulp = require('gulp'),
+    header = require('gulp-header'),
+    less = require('gulp-less'),
+    rename = require('gulp-rename'),
+    eslint = require('gulp-eslint');
 
 // paths of the packages for the compile-task
 var pkgs = [
-    { path: 'app/installer/', data: '../../composer.json' },
-    { path: 'app/system/modules/theme/', data: '../../../../composer.json' },
-    { path: 'themes/one/', data: 'composer.json' }
+    {path: 'themes/bixblack/', data: 'composer.json'}
 ];
 
 // banner for the css files
-var banner = "/*! <%= data.title %> <%= data.version %> | (c) 2014 Pagekit | MIT License */\n";
+var banner = "/*! <%= data.title %> <%= data.version %> | (c) 2015 Pagekit/Bixie | MIT License */\n";
 
 gulp.task('default', ['compile']);
 
@@ -30,16 +28,15 @@ gulp.task('default', ['compile']);
  */
 gulp.task('compile', function () {
 
-    return merge.apply(null, pkgs.map(function (pkg) {
-        return gulp.src(pkg.path + '**/less/*.less', {base: pkg.path})
-            .pipe(less({ compress: true, relativeUrls: true }))
-            .pipe(header(banner, { data: require('./' + pkg.path + pkg.data) }))
-            .pipe(rename(function (file) {
-                // the compiled less file should be stored in the css/ folder instead of the less/ folder
-                file.dirname = file.dirname.replace('less', 'css');
-            }))
-            .pipe(gulp.dest(pkg.path));
-    }));
+    return merge.apply(null, gulp.src('./less/*.less', {base: './'})
+        .pipe(less({compress: true, relativeUrls: true}))
+        .pipe(header(banner, {data: require('./composer.json')}))
+        .pipe(rename(function (file) {
+            // the compiled less file should be stored in the css/ folder instead of the less/ folder
+            file.dirname = file.dirname.replace('less', 'css');
+        }))
+        .pipe(gulp.dest('./'))
+        );
 
 });
 
@@ -48,21 +45,4 @@ gulp.task('compile', function () {
  */
 gulp.task('watch', function () {
     gulp.watch('**/*.less', ['compile']);
-});
-
-/**
- * Lint all script files
- */
-gulp.task('lint', function () {
-    return gulp.src([
-            'app/modules/**/*.js',
-            'app/system/**/*.js',
-            'extensions/**/*.js',
-            'themes/**/*.js',
-            '!**/bundle/*',
-            '!**/vendor/**/*'
-        ])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failOnError());
 });
