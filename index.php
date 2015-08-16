5562e93843fbc417f24b1f7d85d93e402d0a8360
malles <matthijs@bixie.nl>
2015-08-16 20:12:56 +0200

    cleanup

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 return [
 
-    'name' => 'bixblack',
+	'name' => 'bixblack',
 
-    'type' => 'theme',
+	'type' => 'theme',
 
 	'main' => 'Pagekit\\Bixie\\BixieBlackTheme',
 
@@ -15,101 +15,81 @@ return [
 	],
 
 	/**
-     * Resourcessda
-     */
-    'resources' => [
+	 * Resourcessda
+	 */
+	'resources' => [
 
-        'theme:' => '',
-        'views:' => 'views'
+		'theme:' => '',
+		'views:' => 'views'
 
-    ],
+	],
 
-    /**
-     * Menu positions
-     */
-    'menus' => [
+	/**
+	 * Menu positions
+	 */
+	'menus' => [
 
-        'main' => 'Main',
-        'offcanvas' => 'Offcanvas'
+		'main' => 'Main',
+		'offcanvas' => 'Offcanvas'
 
-    ],
+	],
 
-    /**
-     * Widget positions
-     */
-    'positions' => [
+	/**
+	 * Widget positions
+	 */
+	'positions' => [
 
-        'hero' => 'Hero',
-        'top' => 'Top',
-        'sidebar' => 'Sidebar',
-        'bottom' => 'Bottom',
-        'footer' => 'Footer',
-        'offcanvas' => 'Offcanvas'
+		'hero' => 'Hero',
+		'top' => 'Top',
+		'sidebar' => 'Sidebar',
+		'bottom' => 'Bottom',
+		'footer' => 'Footer',
+		'offcanvas' => 'Offcanvas'
 
-    ],
+	],
 
-    /**
-     * Node defaults
-     */
-    'node' => [
+	/**
+	 * Node defaults
+	 */
+	'node' => [
 
 		'style' => '',
-        'title_hide' => false,
-        'html_class' => '',
+		'title_hide' => false,
+		'html_class' => '',
 		'fixed_menu' => false,
 		'hero-image' => '',
-        'hero-contrast' => '',
-        'navbar-transparent' => '',
+		'hero-contrast' => '',
+		'navbar-transparent' => '',
 
-    ],
+	],
 
-    /**
-     * Widget defaults
-     */
-    'widget' => [
+	/**
+	 * Widget defaults
+	 */
+	'widget' => [
 
-        'title_hide' => false,
-        'html_class' => '',
-        'panel' => '',
-        'title_size' => 'uk-panel-title',
-        'alignment' => ''
+		'title_hide' => false,
+		'html_class' => '',
+		'panel' => '',
+		'title_size' => 'uk-panel-title',
+		'alignment' => ''
 
-    ],
+	],
 
-    /**
-     * Settings url
-     */
-    'settings' => '@site/settings#site-theme',
+	/**
+	 * Settings url
+	 */
+	'settings' => '@site/settings#site-theme',
 
-    /**
-     * Configuration defaults
-     */
-    'config' => [
+	/**
+	 * Configuration defaults
+	 */
+	'config' => [
 
-        'default_style' => 'default',
-        'fixed_menu' => false
+		'default_style' => 'default',
+		'fixed_menu' => false
 
-    ],
-
-    /**
-     * Styles defaults
-     */
-    'styles' => [
-
-        'default' => [
-			'data' => [
-				'scheme' => 'default',
-				'fixed_menu' => 1
-			]
-		],
-        'gold' => [
-			'data' => [
-				'scheme' => 'gold',
-				'fixed_menu' => 0
-			]
-		]
-
-    ],
+	],
 
 	'routes' => [
 
@@ -123,55 +103,55 @@ return [
 	],
 
 	/**
-     * Events
-     */
-    'events' => [
+	 * Events
+	 */
+	'events' => [
 
-        'view.system/site/admin/settings' => function ($event, $view) use ($app) {
-            $view->script('site-theme', 'theme:app/bundle/site-theme.js', 'site-settings');
-            $view->data('$bixie', [
+		'view.system/site/admin/settings' => function ($event, $view) use ($app) {
+			$view->script('site-theme', 'theme:app/bundle/site-theme.js', 'site-settings');
+			$view->data('$bixie', [
 				'styles' => $this->getStyles()
 			]);
-            $view->data('$theme', $this);
-        },
+			$view->data('$theme', $this);
+		},
 
-        'view.system/site/admin/edit' => function ($event, $view) {
-            $view->script('node-theme', 'theme:app/bundle/node-theme.js', 'site-edit');
-        },
+		'view.system/site/admin/edit' => function ($event, $view) {
+			$view->script('node-theme', 'theme:app/bundle/node-theme.js', 'site-edit');
+		},
 
-        'view.system/widget/edit' => function ($event, $view) {
-            $view->script('widget-theme', 'theme:app/bundle/widget-theme.js', 'widget-edit');
-        },
+		'view.system/widget/edit' => function ($event, $view) {
+			$view->script('widget-theme', 'theme:app/bundle/widget-theme.js', 'widget-edit');
+		},
 
-        /**
-        * Custom markup calculations based on theme settings
-        */
-        'view.layout' => function ($event, $view) use ($app) {
+		/**
+		 * Custom markup calculations based on theme settings
+		 */
+		'view.layout' => function ($event, $view) use ($app) {
 
-            if ($app->isAdmin()) {
-                return;
-            }
+			if ($app->isAdmin()) {
+				return;
+			}
 
-            $classes = [
-                'navbar' => 'tm-navbar'
-            ];
+			$classes = [
+				'navbar' => 'tm-navbar'
+			];
 
-            $sticky = [
-                'media' => 767,
-                'showup' => true,
-                'animation' => 'uk-animation-slide-top'
-            ];
+			$sticky = [
+				'media' => 767,
+				'showup' => true,
+				'animation' => 'uk-animation-slide-top'
+			];
 
 			if (empty($event['style'])) {
 				$event['style'] = $event['default_style'];
 			}
 
-            $classes['sticky'] = $event['fixed_menu'] ? 'data-uk-sticky=\''.json_encode($sticky).'\'' : '';
+			$classes['sticky'] = $event['fixed_menu'] ? 'data-uk-sticky=\'' . json_encode($sticky) . '\'' : '';
 
-            $event->addParameters(['classes' => $classes]);
+			$event->addParameters(['classes' => $classes]);
 
-        }
+		}
 
-    ]
+	]
 
 ];
