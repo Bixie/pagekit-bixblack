29829a02d397e0bc359238a82fc27f73b4da0c72
malles <matthijs@bixie.nl>
2016-04-04 15:08:53 +0200

    fix menu params

--- views/system/site/menu.php
+++ views/system/site/menu.php
@@ -1,19 +1,19 @@
 <?php if ($root->getDepth() === 0) : ?>
-<ul class="<?= implode(' ', (array) $params['class']) ?>">
-<?php endif ?>
+<ul class="<?= implode(' ', (!empty($class) ? (array) $class : [])) ?>">
+	<?php endif ?>
 
-    <?php foreach ($root->getChildren() as $node) : ?>
-    <li class="<?= $node->hasChildren() ? 'uk-parent' : '' ?><?= $node->get('active') ? ' uk-active' : '' ?>">
-        <a href="<?= $node->getUrl() ?>"><?= $node->title ?></a>
+	<?php foreach ($root->getChildren() as $node) : ?>
+		<li class="<?= $node->hasChildren() ? 'uk-parent' : '' ?><?= $node->get('active') ? ' uk-active' : '' ?>">
+			<a href="<?= $node->getUrl() ?>"><?= $node->title ?></a>
 
-        <?php if ($node->hasChildren()) : ?>
-        <ul class="uk-nav-sub">
-            <?= $view->render('system/site/menu.php', ['root' => $node]) ?>
-        </ul>
-        <?php endif ?>
-    </li>
-    <?php endforeach ?>
+			<?php if ($node->hasChildren() and !empty($depth) and $root->getDepth() < $depth) : ?>
+				<ul class="uk-nav-sub">
+					<?= $view->render('system/site/menu.php', ['root' => $node]) ?>
+				</ul>
+			<?php endif ?>
+		</li>
+	<?php endforeach ?>
 
-<?php if ($root->getDepth() === 0) : ?>
+	<?php if ($root->getDepth() === 0) : ?>
 </ul>
 <?php endif ?>
